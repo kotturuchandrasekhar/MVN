52bc07232aa89b3400589b5a38dd53d3f1270f56
sanju <sanju@LAPTOP-95K17NKT>
2024-10-23 09:56:51 +0530

    First Commit

---
+++ MVN/src/test/java/com/actitime/testscript/CustomerModule.java
@@ -0,0 +1,17 @@
+package com.actitime.testscript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+	
+	@Test
+	public void createCustomer()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.close();
+	}
+
+}
